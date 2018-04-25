1bcbaaa79c89f2912a8a90097424b8469eac5a5f
Vsevolod Shchur <sevaxxx.sx@gmail.com>
2018-04-25 21:44:35 +0500

    game.cs

---
+++ Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PacmanProject
+{
+    public class Game
+    {
+        public static Cell[,] Map;
+        public static int MapWidth => Map.GetLength(0);
+        public static int MapHeight => Map.GetLength(1);
+        public static bool IsOver;
+    }
+
+    public enum Cell
+    {
+        wall,
+        empty
+    }
+}
