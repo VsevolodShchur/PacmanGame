d8d4462fd9bcb3152b21da135eedf944b474c398
Vsevolod Shchur <sevaxxx.sx@gmail.com>
2018-04-25 17:28:04 +0500

    1
    first

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacmanProject
+{
+    class Program
+    {
+        static void Main()
+        {
+        //bla bla
+            var a = int.Parse(Console.ReadLine());
+            for (var i=0; i<=a;i++)
+                Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
